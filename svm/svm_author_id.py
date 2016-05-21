79f7808e834020495c1ec9247c2529b61a2fc8e0
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-22 01:57:30 +0700

    Train SVM classifer and predict

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+t0 = time()
+cls = SVC(kernel='linear')
+cls.fit(features_train, labels_train)
+print("training time:", round(time() - t0, 3), "s")
 
+t1 = time()
+pred = cls.predict(features_test)
+print('prediction time:', round(time() - t1, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
 #########################################################
-
-
