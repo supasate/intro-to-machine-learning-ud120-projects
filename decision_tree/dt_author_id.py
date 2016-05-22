2453ad8fc1cdcea99b7f1df5cf2f54d3370a7540
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-22 09:16:27 +0700

    Train a DecisionTreeClassifier and predict

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,8 +24,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+cls = DecisionTreeClassifier(min_samples_split = 40)
+t0 = time()
+cls.fit(features_train, labels_train)
+print("training time", round(time() - t0, 3))
 
+t1 = time()
+pred = cls.predict(features_test)
+print("prediction time", round(time() - t1, 3))
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print("accuracy", accuracy)
 
 #########################################################
-
-
