3e19b47dc8a5db79341ed463fc6720abc3a0bc2b
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-23 16:13:57 +0700

    Find number of people, number of features, number of poi, and total stock value of James Prentice

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,23 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print("number of people", len(enron_data))
+print("number of features", len(enron_data["SKILLING JEFFREY K"]))
 
+num_person_of_interest = 0
+for name, features_dict in enron_data.items():
+    if features_dict["poi"] == 1:
+        num_person_of_interest += 1
+print("number of poi", num_person_of_interest)
 
+# Show name list
+#print(enron_data.keys())
+
+# Show total stock value of James Prentice
+print(enron_data["PRENTICE JAMES"]["total_stock_value"])
