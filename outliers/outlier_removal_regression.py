8a11b5ac41812813727612a7039fde26fd634e1c
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-31 22:33:10 +0700

    Slope and accuracy before removeing outliers

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,9 +26,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print("slope", reg.coef_)
 
-
+accuracy = reg.score(ages_test, net_worths_test)
+print("accuracy", accuracy)
 
 
 
@@ -81,4 +86,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
