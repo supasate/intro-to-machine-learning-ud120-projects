654cd3ab056014cf27899d1168be41cec3938854
Supasate Choochaisri <supasate.c@gmail.com>
2016-06-01 08:24:10 +0700

    Perform feature scaling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -50,7 +50,7 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -59,7 +59,18 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
+    print(f1, f2)
+    plt.scatter( f1, f2 )
+plt.show()
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+finance_features = scaler.fit_transform(finance_features)
+for f1, f2 in finance_features:
+    print(f1, f2)
     plt.scatter( f1, f2 )
 plt.show()
 
